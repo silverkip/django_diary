7335956fc361ce371b06bf47585cbebc90c96c62
silverkip <xilvkip@gmail.com>
2019-11-29 23:42:38 +0900

    Added basic user functionality

--- diaries/views.py
+++ diaries/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Diary
+from .models import Diary, User
 from django.http import Http404
 from datetime import datetime
+from django.contrib import messages
 
 def top(request): #Top page
     return render(request, 'diaries/top.html')
@@ -15,38 +16,53 @@ def index(request):
 
 def write(request):
     if request.method == "POST":
+        user_id = request.POST['editing_user']
+        user = User.objects.get(pk=user_id)
         entry = Diary.objects.create(
             title=request.POST['title'],
             date=request.POST['time'],
             created=datetime.now(),
             content=request.POST['content'],
+            user=user,
             )
+        messages.success(request, "Entry Posted!")
         return redirect('detail', entry.id)
-    return render(request, 'diaries/write.html')
+    context = {
+        'users' : User.objects.order_by('id'),
+    }
+    return render(request, 'diaries/write.html', context)
 
 def update(request, diary_id):
     try:
         entry = Diary.objects.get(pk=diary_id)
+        user = entry.user
     except Diary.DoesNotExist:
         raise Http404("Entry does not exist.")
     if request.method == "POST":
-        entry.content = request.POST['content']
-        entry.title = request.POST['title']
-        entry.edited = datetime.now()
-        entry.save()
-        return redirect('detail', diary_id)
+        if request.POST['editing_user'] != str(user.id):
+            messages.error(request, "You're not the owner of this diary entry!")
+        else:
+            entry.content = request.POST['content']
+            entry.title = request.POST['title']
+            entry.edited = datetime.now()
+            entry.save()
+            messages.success(request, "Successfully edited your entry.")
+            return redirect('detail', diary_id)
     context = {
         'entry': entry,
+        'users': User.objects.order_by('id'),
     }
     return render(request, 'diaries/update.html', context)
 
 def details(request, diary_id):
     try:
         entry = Diary.objects.get(pk=diary_id)
+        user = entry.user
     except Diary.DoesNotExist:
         raise Http404("Entry does not exist.")
     context = {
         'entry': entry,
+        'user' : user,
     }
     return render(request, 'diaries/detail.html', context)
 
@@ -56,4 +72,5 @@ def delete(request, diary_id):
     except Diary.DoesNotExist:
         raise Http404("Entry does not exist.")
     entry.delete()
+    messages.success(request, "Entry Deleted Successfully")
     return redirect('entries')
